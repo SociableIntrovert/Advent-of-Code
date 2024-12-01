e0f131b2a9afff15c7f80d52411ec33c56e6a4ce
Ryon O'Mara <frankenteg@hotmail.com>
2024-11-30 18:38:51 -0600

    Completed 2015 Day 2

---
+++ 2015/Day_2/Part_2/part_2.py
@@ -0,0 +1,29 @@
+
+
+    
+
+with open('../input', 'r') as file:
+    gifts = [line.strip() for line in file]
+
+total_ribbon = 0
+
+# 0 = length
+# 1 = width
+# 2 = height
+
+# 2 * 0 * 1
+# 2 * 1 * 2
+# 2 * 2 * 0
+
+for gift in gifts:
+    values = gift.split('x')
+    values[0] = int(values[0])
+    values[1] = int(values[1])
+    values[2] = int(values[2])
+    values.sort()
+    total_ribbon += (values[0] * 2) + (values[1] * 2) + (values[0] * values[1] * values[2])
+    # print(values)
+    # total_paper += (2 * (values[0] * values[1])) + (2 * (values[1] * values[2])) + (2 * (values[2] * values[0])) + (min([values[0] * values[1], values[1] * values[2], values[2] * values[0]]))
+
+
+print(total_ribbon)
