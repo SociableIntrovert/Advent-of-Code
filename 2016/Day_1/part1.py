49249590c382633430a3f8ad7d582150681652df
romara-uno <romara@unomaha.edu>
2025-12-01 14:20:28 -0600

    Completed Day 1 Part 1 2016

--- 2016/Day_1/part1.py
+++ 2016/Day_1/part1.py
@@ -1,8 +1,41 @@
+def convert_direction(curr_pos, instrunction):
+    if curr_pos['current_dir'] == 'N':
+        if instrunction[0:1] == 'L':
+            curr_pos['current_dir'] = 'W'
+            curr_pos['x'] -= int(instrunction[1:])
+        else:
+            curr_pos['current_dir'] = 'E'
+            curr_pos['x'] += int(instrunction[1:])
+    elif curr_pos['current_dir'] == 'E':
+        if instrunction[0:1] == 'L':
+            curr_pos['current_dir'] = 'N'
+            curr_pos['y'] += int(instrunction[1:])
+        else:
+            curr_pos['current_dir'] = 'S'
+            curr_pos['y'] -= int(instrunction[1:])
+    elif curr_pos['current_dir'] == 'S':
+        if instrunction[0:1] == 'L':
+            curr_pos['current_dir'] = 'E'
+            curr_pos['x'] += int(instrunction[1:])
+        else:
+            curr_pos['current_dir'] = 'W'
+            curr_pos['x'] -= int(instrunction[1:])
+    elif curr_pos['current_dir'] == 'W':
+        if instrunction[0:1] == 'L':
+            curr_pos['current_dir'] = 'S'
+            curr_pos['y'] -= int(instrunction[1:])
+        else:
+            curr_pos['current_dir'] = 'N'
+            curr_pos['y'] += int(instrunction[1:])
+
+    return curr_pos
+
+
 directions = []
 starting_pos = {
     'x': 0,
     'y': 0,
-    'current_dir': None
+    'current_dir': 'N'
 }
 
 with open('input', 'r') as file:
@@ -13,4 +46,7 @@ with open('input', 'r') as file:
         directions[index] = directions[index].strip()
 
 for direction in directions:
+    starting_pos = convert_direction(starting_pos, direction)
+
+print(starting_pos)
     
